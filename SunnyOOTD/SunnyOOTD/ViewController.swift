74cb875a66ddf8f352798c2d48ebfa6a0dbe198a
Hyeon <81559117+SooHoon95@users.noreply.github.com>
2023-05-24 15:46:12 +0900

    CREATE PROJECT

---
+++ SunnyOOTD/SunnyOOTD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SunnyOOTD
+//
+//  Created by 최수훈 on 2023/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
