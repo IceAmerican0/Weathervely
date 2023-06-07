decbf4ac51cb878764236ca8c3819b02f3a1d598
IceAmerican0 <starsjun@naver.com>
2023-06-07 23:09:06 +0900

    ADD:] SPM & Foldering & BaseViewController 작업중..
     - FlexLayout, PinLayout, Then, RxSwift 추가
     - 누락된 LaunchScreen 원복

---
+++ Weathervely/Weathervely/Helper/Base/BaseViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BaseViewController.swift
+//  Weathervely
+//
+//  Created by 박성준 on 2023/06/06.
+//
+
+import UIKit
+import RxSwift
+import Then
+import FlexLayout
+import PinLayout
+
+class BaseViewController: UIViewController {
+    
+    var container = UIView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+    func configureUI() {
+        view.addSubview(container)
+        
+        container.pin.all()
+        container.flex.layout()
+    }
+    
+}
--- Weathervely/Weathervely/View/HomeViewController.swift
+++ Weathervely/Weathervely/View/HomeViewController.swift
@@ -5,12 +5,17 @@
 //  Created by 박성준 on 2023/06/06.
 //
 
-import Foundation
+import UIKit
 
 class HomeViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    override func configureUI() {
         
     }
 }
