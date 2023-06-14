5b09742622caacfdd11118b761587b6c0cdcd538
SooHoon <dkehskeh@gmail.com>
2023-06-14 19:09:23 +0900

    FIX:] Pinlatout Edge 사용해서 layout 정의
    다른뷰의 위치 기준으로 거리 정의하는 메소드로 수정

--- Weatherbly/Weatherbly/Feature/OnBoard/View/OnBoardViewController.swift
+++ Weatherbly/Weatherbly/Feature/OnBoard/View/OnBoardViewController.swift
@@ -10,10 +10,11 @@ import Then
 
 final class OnBoardViewController: BaseViewController {
     
-    private lazy var topGreetingLabel = UILabel()
-    private lazy var logo = UIImageView()
-    private lazy var bottomGreetingLabel = UILabel()
-    private lazy var startButton = CSButton(.primary)
+    private var contentView = UIView()
+    private var topGreetingLabel = UILabel()
+    private var logo = UIImageView()
+    private var bottomGreetingLabel = UILabel()
+    private var startButton = CSButton(.primary)
     
     
     override func attribute() {
@@ -23,19 +24,19 @@ final class OnBoardViewController: BaseViewController {
             $0.text = "날씨블리에 오신 걸\n환영해요!"
             $0.numberOfLines = 0
             $0.textAlignment = .center
-            $0.font = .systemFont(ofSize: 25)
+            $0.font = .boldSystemFont(ofSize: 25)
         }
         
         logo.do {
             $0.image = UIImage(systemName: "star.fill")
+            
         }
         
         bottomGreetingLabel.do {
             $0.text = "날씨블리가 날씨에 맞는\n옷차림을 알려드릴 거에요"
             $0.numberOfLines = 0
             $0.textAlignment = .center
-            $0.font = .systemFont(ofSize: 25)
-
+            $0.font = .boldSystemFont(ofSize: 20)
             
         }
         
@@ -47,6 +48,10 @@ final class OnBoardViewController: BaseViewController {
         
     }
     override func layout() {
+        
+        contentView.pin.all(container.pin.safeArea)
+        contentView.backgroundColor = .yellow
+        
     
         container.flex.alignItems(.center).define { flex in
             flex.addItem(topGreetingLabel)
@@ -55,12 +60,13 @@ final class OnBoardViewController: BaseViewController {
             flex.addItem(startButton).width(304).height(62)
         }
         
+    /// 비율? 인지 모든 디바이스에서 어떻게 작동하는 것이 옳은것인지 더 찾아볼 필요가있다.
+    /// CGFloat 픽셀 기반이라 기본적으로 다른 디바이스에서도 똑같이 보이는게 맞는데
         topGreetingLabel.pin.top(view.pin.safeArea.top + 93).left(30).right(30)
-        logo.pin.top(view.pin.safeArea.top + 224).left(126).right(126)
-        bottomGreetingLabel.pin.top(view.pin.safeArea.top + 652).left(95).right(95)
-        startButton.pin.top(view.pin.safeArea.top + 652).left(43).right(43)
+        logo.pin.top(to: topGreetingLabel.edge.bottom).marginTop(58).left(126).right(126)
+        bottomGreetingLabel.pin.top(to: logo.edge.bottom).marginTop(100).left(95).right(95)
+        startButton.pin.top(to: bottomGreetingLabel.edge.bottom).marginTop(69).left(43).right(43)
     
-//        .justifyContent(.spaceEvenly)
     }
     
     @objc private func didTapStartButton() {
