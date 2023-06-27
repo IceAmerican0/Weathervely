3a61007f6f4d97d1c6c28b4cfee1184aa819dd32
IceAmerican0 <starsjun@naver.com>
2023-06-27 17:58:41 +0900

    ADD:] keyboard hide/show 버튼 위치 조정

--- Weatherbly/Weatherbly/Feature/OnBoard/View/NicknameViewController.swift
+++ Weatherbly/Weatherbly/Feature/OnBoard/View/NicknameViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FlexLayout
+import PinLayout
 
 final class NicknameViewController: BaseViewController {
     
@@ -15,7 +16,28 @@ final class NicknameViewController: BaseViewController {
     private var explanationLabel = CSLabel(.bold, 25, "닉네임을 설정해주세요")
     private var guideLabel = CSLabel(.bold, 20, "(5글자 이내)")
     private var inputNickname = UITextField()
+    private var buttonWrapper = UIView()
     private var confirmButton = CSButton(.primary)
+    
+    private let buttonMarginBottom = UIScreen.main.bounds.height * 0.1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(buttonWrapper)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        inputNickname.becomeFirstResponder()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        buttonWrapper.pin.all(view.pin.safeArea)
+        buttonWrapper.flex.layout()
+    }
     
     override func attribute() {
         super.attribute()
@@ -49,11 +71,31 @@ final class NicknameViewController: BaseViewController {
             flex.addItem(explanationLabel).marginTop(27)
             flex.addItem(guideLabel)
             flex.addItem(inputNickname).marginTop(36).width(330).height(50)
+        }
+        
+        buttonWrapper.flex.justifyContent(.end).alignItems(.center)
+            .marginBottom(buttonMarginBottom)
+            .define { flex in
             flex.addItem(confirmButton).width(88%).height(62)
         }
     }
     
     @objc private func didTapConfirmButton() {
         
+    }
+    
+    // MARK: Keyboard Action
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                buttonWrapper.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if buttonWrapper.frame.origin.y != buttonMarginBottom {
+            buttonWrapper.frame.origin.y = buttonMarginBottom
+        }
     }
 }
