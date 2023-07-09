45a00f190b79d3bc98900560313af2de85d8349c
SooHoon <dkehskeh@gmail.com>
2023-07-09 17:18:55 +0900

    EdintNicknameViewController:] 생성

---
+++ Weatherbly/Weatherbly/Feature/EditNicknameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EditNicknameViewController.swift
+//  Weatherbly
+//
+//  Created by 최수훈 on 2023/07/09.
+//
+
+import UIKit
+import FlexLayout
+import PinLayout
+
+final class EditNicknameViewController: BaseViewController {
+    
+    var csNavigationView = CSNavigationView(.leftButton(AssetsImage.navigationBackButton.image))
+    
+}
