f27277243f83ca5b3a019a7d4f3559c94df83864
IceAmerican0 <starsjun@naver.com>
2023-10-19 22:24:49 +0900

    [Experiment] 콜렉션뷰 적용 완료

--- Weatherbly/Weatherbly/Feature/Trend/TrendingCollectionViewCell.swift
+++ Weatherbly/Weatherbly/Feature/Trend/TrendingCollectionViewCell.swift
@@ -12,8 +12,7 @@ import Then
 
 final class TrendingCollectionViewCell: UICollectionViewCell {
     private let container = UIView()
-    private let imageView = UIImageView()
-    private let textLabel = CSLabel(.regular, 11, "loading...")
+    let imageView = UIImageView()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -27,11 +26,20 @@ final class TrendingCollectionViewCell: UICollectionViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         contentView.flex.layout()
-        
     }
     
     func layout() {
+        self.backgroundColor = .white
+        contentView.flex.alignItems(.center).justifyContent(.center).define {
+            $0.addItem(imageView).width(((UIScreen.main.bounds.width - 30) / 3) - 20).height(140)
+        }
         
+        self.layer.borderColor = UIColor.clear.cgColor
+        self.layer.borderWidth = 1
+        self.contentView.layer.shadowColor = CSColor._255_255_255_05.cgColor
+        self.layer.setShadow(CGSize(width: 0, height: 4), CSColor.none.cgColor, 0.25, 7.5)
+        self.layer.masksToBounds = false
+        self.clipsToBounds = false
     }
     
     func configureCellState() {
--- Weatherbly/Weatherbly/Feature/Trend/TrendingViewModel.swift
+++ Weatherbly/Weatherbly/Feature/Trend/TrendingViewModel.swift
@@ -6,11 +6,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 protocol TrendingViewModelLogic: ViewModelBusinessLogic {
-    
+    func getRecommendCloset()
 }
 
 final class TrendingViewModel: RxBaseViewModel, TrendingViewModelLogic {
+    let recommendClosetEntityRelay = BehaviorRelay<[RecommendClosetInfo]>(value: [])
     
+    public func getRecommendCloset() {
+        let getClosetDataSource = ClosetDataSource()
+        getClosetDataSource.getRecommendCloset(Date().todayHourFormat)
+            .subscribe(
+                with: self,
+                onNext: { owner, response in
+                    guard let closets = response.data?.list.closets else { return }
+                    var temp: [RecommendClosetInfo] = []
+                    for _ in 0..<5 {
+                        temp += closets
+                    }
+                    owner.recommendClosetEntityRelay.accept(temp)
+                },
+                onError: { owner, error in
+                    owner.alertMessageRelay.accept(.init(title: error.localizedDescription,
+                                                         alertType: .Error,
+                                                         closeAction: {
+                        owner.navigationPopToSelfRelay.accept(Void())
+                    }))
+            })
+            .disposed(by: bag)
+    }
 }
