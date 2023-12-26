a5aa36abfd4ceeaf5052390e71c3a191c5c9605a
IceAmerican0 <starsjun@naver.com>
2023-12-26 17:59:45 +0900

    [디자인 시스템] 폰트 선언 완료 / 컬러 진행중
     - assets 파일명 수정

--- Weatherbly/Weatherbly/Helper/Common/Extension/UIColor+Extension.swift
+++ Weatherbly/Weatherbly/Helper/Common/Extension/UIColor+Extension.swift
@@ -32,3 +32,93 @@ extension UIColor {
     }
     
 }
+
+// MARK: Custom Color
+public extension UIColor {
+    static let blue10 = UIColor(named: "blue10")
+    static let blue50 = UIColor(named: "blue50")
+    static let blue100 = UIColor(named: "blue100")
+    static let blue150 = UIColor(named: "blue150")
+    static let blue200 = UIColor(named: "blue200")
+    static let blue300 = UIColor(named: "blue300")
+    static let blue400 = UIColor(named: "blue400")
+    static let blue500 = UIColor(named: "blue500")
+    static let blue600 = UIColor(named: "blue600")
+    static let blue700 = UIColor(named: "blue700")
+    static let blue800 = UIColor(named: "blue800")
+    static let blue900 = UIColor(named: "blue900")
+    
+    static let violet10 = UIColor(named: "violet10")
+    static let violet50 = UIColor(named: "violet50")
+    static let violet100 = UIColor(named: "violet100")
+    static let violet150 = UIColor(named: "violet150")
+    static let violet200 = UIColor(named: "violet200")
+    static let violet300 = UIColor(named: "violet300")
+    static let violet400 = UIColor(named: "violet400")
+    static let violet500 = UIColor(named: "violet500")
+    static let violet600 = UIColor(named: "violet600")
+    static let violet700 = UIColor(named: "violet700")
+    static let violet800 = UIColor(named: "violet800")
+    static let violet900 = UIColor(named: "violet900")
+    
+    static let gray10 = UIColor(named: "gray10")
+    static let gray20 = UIColor(named: "gray20")
+    static let gray30 = UIColor(named: "gray30")
+    static let gray40 = UIColor(named: "gray40")
+    static let gray50 = UIColor(named: "gray50")
+    static let gray60 = UIColor(named: "gray60")
+    static let gray70 = UIColor(named: "gray70")
+    static let gray80 = UIColor(named: "gray80")
+    static let gray90 = UIColor(named: "gray90")
+    static let gray100 = UIColor(named: "gray100")
+    static let gray200 = UIColor(named: "gray200")
+    static let gray300 = UIColor(named: "gray300")
+    static let gray400 = UIColor(named: "gray400")
+    static let gray500 = UIColor(named: "gray500")
+    static let gray600 = UIColor(named: "gray600")
+    static let gray700 = UIColor(named: "gray700")
+    static let gray800 = UIColor(named: "gray800")
+    static let gray900 = UIColor(named: "gray900")
+    
+    static let red10 = UIColor(named: "red10")
+    static let red50 = UIColor(named: "red50")
+    static let red100 = UIColor(named: "red100")
+    static let red150 = UIColor(named: "red150")
+    static let red200 = UIColor(named: "red200")
+    static let red300 = UIColor(named: "red300")
+    static let red400 = UIColor(named: "red400")
+    static let red500 = UIColor(named: "red500")
+    static let red600 = UIColor(named: "red600")
+    static let red700 = UIColor(named: "red700")
+    static let red800 = UIColor(named: "red800")
+    static let red900 = UIColor(named: "red900")
+    
+    static let salmon10 = UIColor(named: "salmon10")
+    static let salmon50 = UIColor(named: "salmon50")
+    static let salmon100 = UIColor(named: "salmon100")
+    static let salmon150 = UIColor(named: "salmon150")
+    static let salmon200 = UIColor(named: "salmon200")
+    static let salmon300 = UIColor(named: "salmon300")
+    static let salmon400 = UIColor(named: "salmon400")
+    static let salmon500 = UIColor(named: "salmon500")
+    static let salmon600 = UIColor(named: "salmon600")
+    static let salmon700 = UIColor(named: "salmon700")
+    static let salmon800 = UIColor(named: "salmon800")
+    static let salmon900 = UIColor(named: "salmon900")
+    
+    static let orange10 = UIColor(named: "orange10")
+    
+    static let yellow10 = UIColor(named: "yellow10")
+    
+    static let pistachio10 = UIColor(named: "pistachio10")
+    
+    static let green10 = UIColor(named: "green10")
+    
+    static let mint10 = UIColor(named: "mint10")
+    
+    static let sky10 = UIColor(named: "sky10")
+    
+    static let lightBlue10 = UIColor(named: "lightBlue10")
+    
+    static let pink10 = UIColor(named: "pink10")
+}
--- Weatherbly/Weatherbly/Helper/Enum/CSFont.swift
+++ Weatherbly/Weatherbly/Helper/Enum/CSFont.swift
@@ -5,32 +5,120 @@
 //  Created by 최수훈 on 12/24/23.
 //
 
-import Foundation
 import UIKit
 
-/*
- AppleSDGothicNeoB
- AppleSDGothicNeoEB
- AppleSDGothicNeoH
- AppleSDGothicNeoL
- AppleSDGothicNeoM
- AppleSDGothicNeoR
- AppleSDGothicNeoSB
- AppleSDGothicNeoT
- AppleSDGothicNeoUL
- */
+private enum GothicNeo: String {
+    case heavy      = "AppleSDGothicNeoH"
+    case extraBold  = "AppleSDGothicNeoEB"
+    case bold       = "AppleSDGothicNeoB"
+    case semiBold   = "AppleSDGothicNeoSB"
+    case medium     = "AppleSDGothicNeoM"
+    case regular    = "AppleSDGothicNeoR"
+    case light      = "AppleSDGothicNeoL"
+    case ultraLight = "AppleSDGothicNeoUL"
+    case thin       = "AppleSDGothicNeoT"
+}
 
-enum CSFont {
+public enum CSFont {
     case heading_1_UL
     case heading_2_B
+    case heading_3_B
+    case heading_4_B
+    case heading_5_B
+    case title_1_B
+    case title_1_M
+    case title_2_B
+    case title_2_M
+    case title_3_B
+    case title_3_M
+    case body_1_B
+    case body_1_M
+    case body_2_B
+    case body_2_M
+    case body_3_B
+    case body_3_M
+    case body_4_B
+    case body_4_M
+    case body_5_B
+    case body_5_M
+    case caption_1_M
+    case caption_2_M
     
-    var CSFontL: UIFont {
+    var font: UIFont {
+        var name: GothicNeo
+        var size: CGFloat
         switch self {
         case .heading_1_UL:
-            UIFont(name: "AppleSDGothicNeoUL", size: 56) ?? UIFont.systemFont(ofSize: 15)
+            name = GothicNeo.ultraLight
+            size = 56
         case .heading_2_B:
-            UIFont(name: "Apple", size: 32)!
-            
+            name = GothicNeo.bold
+            size = 32
+        case .heading_3_B:
+            name = GothicNeo.bold
+            size = 28
+        case .heading_4_B:
+            name = GothicNeo.bold
+            size = 24
+        case .heading_5_B:
+            name = GothicNeo.bold
+            size = 22
+        case .title_1_B:
+            name = GothicNeo.bold
+            size = 20
+        case .title_1_M:
+            name = GothicNeo.medium
+            size = 20
+        case .title_2_B:
+            name = GothicNeo.bold
+            size = 18
+        case .title_2_M:
+            name = GothicNeo.medium
+            size = 18
+        case .title_3_B:
+            name = GothicNeo.bold
+            size = 17
+        case .title_3_M:
+            name = GothicNeo.medium
+            size = 17
+        case .body_1_B:
+            name = GothicNeo.bold
+            size = 16
+        case .body_1_M:
+            name = GothicNeo.medium
+            size = 16
+        case .body_2_B:
+            name = GothicNeo.bold
+            size = 15
+        case .body_2_M:
+            name = GothicNeo.medium
+            size = 15
+        case .body_3_B:
+            name = GothicNeo.bold
+            size = 14
+        case .body_3_M:
+            name = GothicNeo.medium
+            size = 14
+        case .body_4_B:
+            name = GothicNeo.bold
+            size = 13
+        case .body_4_M:
+            name = GothicNeo.medium
+            size = 13
+        case .body_5_B:
+            name = GothicNeo.bold
+            size = 12
+        case .body_5_M:
+            name = GothicNeo.medium
+            size = 12
+        case .caption_1_M:
+            name = GothicNeo.medium
+            size = 11
+        case .caption_2_M:
+            name = GothicNeo.medium
+            size = 10
         }
+        
+        return UIFont(name: name.rawValue, size: size) ?? UIFont.systemFont(ofSize: 15)
     }
 }
