02c483513b37ef5f697fdfa91cf71dd4507bab50
SooHoon <dkehskeh@gmail.com>
2023-12-30 03:03:22 +0900

    [디자인시스템] NewCSButton 완성
    NewCSButton => CSButton 으로 대체하고
    bgColor, titleColor, font, radius, titleColor를 정의한다.
    
    type으로 크기를
    style로 색상을 control한다.

--- Weatherbly/Weatherbly/Design System/Atom/NewCSButton.swift
+++ Weatherbly/Weatherbly/Design System/Atom/NewCSButton.swift
@@ -12,34 +12,69 @@ import FlexLayout
  
  
  issue 확인해야할 것
- case w 300 h 30 -> CSButton 내에서 지정해줬을떄 -> flex -> 1. 반쪽짜리 2개 가능?
-                                                       2. flex 에서 패딩만 줬을 떄, 버튼 크기 조절됌?
+ case w 300 h 30 -> CSButton 내에서 지정해줬을떄 -> flex - > 1. 반쪽짜리 2개 가능?
+ 2. flex 에서 패딩만 줬을 떄, 버튼 크기 조절됌?
  */
 
 
 
 final public class NewCSButton: UIButton {
     
-    enum Style {
+    // MARK: - Control Property
+    // 버튼 크기에 따라
+    enum ButtonType {
+        case standard
+        case compact
+    }
+    
+    // 버튼 색상에 따라
+    enum ButtonStyle {
         case violet600
         case violet100
         case white // -> border
     }
     
-    init (_ style: Style) {
+    var font = UIFont()
+    
+    init (_ type: ButtonType, style: ButtonStyle) {
         super.init(frame: .zero)
-//        func buttonstyle()
-//        func setradius()
+        buttonConfigure(type, style)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public override func layoutSubviews() {
-//        <#code#>
+    /// bgColor, titleColor, font, radius, titleColor
+    func buttonConfigure(_ type: ButtonType, _ style: ButtonStyle) {
+        
+        if type == .standard {
+            font = UIFont.title_3_B!
+            self.layer.cornerRadius = 12
+            
+            switch style {
+            case .violet600:
+                self.setBackgroundColor(UIColor(resource: .violet600))
+                self.setTitleColor(.white, for: .normal)
+                
+            case .violet100:
+                self.setBackgroundColor(UIColor(resource: .violet100))
+                self.setTitleColor(UIColor(resource: .violet600), for: .normal)
+                
+            case.white:
+                self.setBackgroundColor(.white)
+                self.setTitleColor(UIColor(resource: .violet600), for: .normal)
+                self.layer.borderWidth = 1
+                self.layer.borderColor = UIColor(resource: .violet150).cgColor
+            }
+        } else {
+            font = UIFont.body_2_M!
+            self.layer.cornerRadius = 5
+            self.setBackgroundColor(UIColor(resource: .violet100))
+            self.setTitleColor(UIColor(resource: .violet800), for: .normal)
+        }
+        
     }
-
 }
 
 
--- Weatherbly/Weatherbly/Helper/Enum/UIFont+Extension.swift
+++ Weatherbly/Weatherbly/Helper/Enum/UIFont+Extension.swift
@@ -11,38 +11,38 @@ import UIKit
 public enum GothicNeo {
     static let heavy      = "AppleSDGothicNeoH"
     static let extraBold  = "AppleSDGothicNeoEB"
-    static let bold       = "AppleSDGothicNeoB"
-    static let semiBold   = "AppleSDGothicNeoSB"
-    static let medium     = "AppleSDGothicNeoM"
-    static let regular    = "AppleSDGothicNeoR"
-    static let light      = "AppleSDGothicNeoL"
-    static let ultraLight = "AppleSDGothicNeoUL"
-    static let thin       = "AppleSDGothicNeoT"
+    static let bold       = "AppleSDGothicNeo-Bold"
+    static let semiBold   = "AppleSDGothicNeo-SemiBold"
+    static let medium     = "AppleSDGothicNeo-Medium"
+    static let regular    = "AppleSDGothicNeo-Regular"
+    static let light      = "AppleSDGothicNeo-Light"
+    static let ultraLight = "AppleSDGothicNeo-UltraLight"
+    static let thin       = "AppleSDGothicNeo-Thin"
 }
 
 // MARK: Custom Font
 extension UIFont {
-    static let heading_1_UL = UIFont(name: GothicNeo.ultraLight, size: 67)
-    static let heading_2_B  = UIFont(name: GothicNeo.bold,       size: 40)
-    static let heading_3_B  = UIFont(name: GothicNeo.bold,       size: 36)
-    static let heading_4_B  = UIFont(name: GothicNeo.bold,       size: 32)
-    static let heading_5_B  = UIFont(name: GothicNeo.bold,       size: 30)
-    static let title_1_B    = UIFont(name: GothicNeo.bold,       size: 26)
-    static let title_1_M    = UIFont(name: GothicNeo.medium,     size: 26)
-    static let title_2_B    = UIFont(name: GothicNeo.bold,       size: 24)
-    static let title_2_M    = UIFont(name: GothicNeo.medium,     size: 24)
-    static let title_3_B    = UIFont(name: GothicNeo.bold,       size: 23)
-    static let title_3_M    = UIFont(name: GothicNeo.medium,     size: 23)
-    static let body_1_B     = UIFont(name: GothicNeo.bold,       size: 21)
-    static let body_1_M     = UIFont(name: GothicNeo.medium,     size: 21)
-    static let body_2_B     = UIFont(name: GothicNeo.bold,       size: 20)
-    static let body_2_M     = UIFont(name: GothicNeo.medium,     size: 20)
-    static let body_3_B     = UIFont(name: GothicNeo.bold,       size: 19)
-    static let body_3_M     = UIFont(name: GothicNeo.medium,     size: 19)
-    static let body_4_B     = UIFont(name: GothicNeo.bold,       size: 18)
-    static let body_4_M     = UIFont(name: GothicNeo.medium,     size: 18)
-    static let body_5_B     = UIFont(name: GothicNeo.bold,       size: 17)
-    static let body_5_M     = UIFont(name: GothicNeo.medium,     size: 17)
-    static let caption_1_M  = UIFont(name: GothicNeo.medium,     size: 15)
-    static let caption_2_M  = UIFont(name: GothicNeo.medium,     size: 14)
+    static let heading_1_UL = UIFont(name: GothicNeo.ultraLight, size: 56)
+    static let heading_2_B  = UIFont(name: GothicNeo.bold,       size: 32)
+    static let heading_3_B  = UIFont(name: GothicNeo.bold,       size: 28)
+    static let heading_4_B  = UIFont(name: GothicNeo.bold,       size: 24)
+    static let heading_5_B  = UIFont(name: GothicNeo.bold,       size: 22)
+    static let title_1_B    = UIFont(name: GothicNeo.bold,       size: 20)
+    static let title_1_M    = UIFont(name: GothicNeo.medium,     size: 20)
+    static let title_2_B    = UIFont(name: GothicNeo.bold,       size: 18)
+    static let title_2_M    = UIFont(name: GothicNeo.medium,     size: 18)
+    static let title_3_B    = UIFont(name: GothicNeo.bold,       size: 17)
+    static let title_3_M    = UIFont(name: GothicNeo.medium,     size: 17)
+    static let body_1_B     = UIFont(name: GothicNeo.bold,       size: 16)
+    static let body_1_M     = UIFont(name: GothicNeo.medium,     size: 16)
+    static let body_2_B     = UIFont(name: GothicNeo.bold,       size: 15)
+    static let body_2_M     = UIFont(name: GothicNeo.medium,     size: 15)
+    static let body_3_B     = UIFont(name: GothicNeo.bold,       size: 14)
+    static let body_3_M     = UIFont(name: GothicNeo.medium,     size: 14)
+    static let body_4_B     = UIFont(name: GothicNeo.bold,       size: 13)
+    static let body_4_M     = UIFont(name: GothicNeo.medium,     size: 13)
+    static let body_5_B     = UIFont(name: GothicNeo.bold,       size: 12)
+    static let body_5_M     = UIFont(name: GothicNeo.medium,     size: 12)
+    static let caption_1_M  = UIFont(name: GothicNeo.medium,     size: 11)
+    static let caption_2_M  = UIFont(name: GothicNeo.medium,     size: 10)
 }
