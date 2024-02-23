046da40301c3c483ec988dc97e04da9f3191e983
IceAmerican0 <starsjun@naver.com>
2024-02-23 13:25:44 +0900

    [디자인시스템] CSButton isEnabled 관련 세팅 추가

--- Weatherbly/Weatherbly/DesignSystem/Atom/NewCSButton.swift
+++ Weatherbly/Weatherbly/DesignSystem/Atom/NewCSButton.swift
@@ -10,18 +10,7 @@ import FlexLayout
 import RxGesture
 import RxSwift
 
-/*
- 
- issue 확인해야할 것
- case w 300 h 30 -> CSButton 내에서 지정해줬을떄 -> flex - > 1. 반쪽짜리 2개 가능?
- 2. flex 에서 패딩만 줬을 떄, 버튼 크기 조절됌?
- */
-
-
-
 final public class NewCSButton: UIButton {
-    
-    // MARK: - Control Property
     // 버튼 크기에 따라
     enum ButtonScale {
         case standard
@@ -36,87 +25,97 @@ final public class NewCSButton: UIButton {
     }
     
     var bag = DisposeBag()
-    var font = UIFont()
     
-    init (_ scale: ButtonScale, style: ButtonStyle) {
+    var scale: ButtonScale
+    
+    var style: ButtonStyle
+    
+    init(_ scale: ButtonScale, style: ButtonStyle) {
+        self.scale = scale
+        self.style = style
         super.init(frame: .zero)
-        buttonConfigure(scale, style)
-        setRxBinding(style)
+        buttonConfigure()
+        setRxBinding()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // Button Pressed effect
-    func setRxBinding(_ style: ButtonStyle) {
+    public override var isEnabled: Bool {
+        didSet {
+            buttonConfigure()
+        }
+    }
+    
+    /// Button Pressed effect
+    func setRxBinding() {
         self.rx.controlEvent(.touchDown)
             .asDriver()
             .drive(
                 with: self,
                 onNext: { owner, _ in
-                    switch style {
+                    switch owner.style {
                     case .violet600:
-                        owner.setBackgroundColor(UIColor(resource: .violet400))
+                        owner.setBackgroundColor(.violet400)
                     case .violet100:
-                        owner.setBackgroundColor(UIColor(resource: .violet200))
+                        owner.setBackgroundColor(.violet200)
                     case .white:
-                        owner.setBackgroundColor(UIColor(resource: .violet50))
-                        self.setTitleColor(UIColor(resource: .violet400), for: .highlighted)
+                        owner.setBackgroundColor(.violet50)
+                        self.setTitleColor(.violet400, for: .highlighted)
                     }
-                })
-            .disposed(by: bag)
+                }
+            ).disposed(by: bag)
         
         self.rx.controlEvent([.touchUpInside, .touchUpOutside])
             .asDriver()
             .drive(
                 with: self,
                 onNext: { owner, _ in
-                    switch style {
+                    switch owner.style {
                     case .violet600:
-                        owner.setBackgroundColor(UIColor(resource: .violet600))
+                        owner.setBackgroundColor(.violet600)
                     case .violet100:
-                        owner.setBackgroundColor(UIColor(resource: .violet100))
+                        owner.setBackgroundColor(.violet100)
                     case .white:
                         owner.setBackgroundColor(.white)
-                        self.setTitleColor(UIColor(resource: .violet600), for: .normal)
+                        self.setTitleColor(.violet600, for: .normal)
                     }
-                })
-            .disposed(by: bag)
+                }
+            ).disposed(by: bag)
     }
     
     /// bgColor, titleColor, font, radius, titleColor
-    func buttonConfigure(_ scale: ButtonScale, _ style: ButtonStyle) {
-        
-        self.configuration = .plain()
-        
+    func buttonConfigure() {
         if scale == .standard {
-            font = UIFont.title_3_B
-
+            self.titleLabel?.font = UIFont.title_3_B
             self.layer.cornerRadius = 12
             
             switch style {
             case .violet600:
-                self.setBackgroundColor(UIColor(resource: .violet600))
+                self.backgroundColor = isEnabled ? .violet600 : .gray30
                 self.setTitleColor(.white, for: .normal)
+                self.setTitleColor(.white, for: .disabled)
                 
             case .violet100:
-                self.setBackgroundColor(UIColor(resource: .violet100))
-                self.setTitleColor(UIColor(resource: .violet600), for: .normal)
+                self.backgroundColor = isEnabled ? .violet100 : .gray30
+                self.setTitleColor(.violet600, for: .normal)
+                self.setTitleColor(.white, for: .disabled)
                 
-            case.white:
-                self.setBackgroundColor(.white)
-                self.setTitleColor(UIColor(resource: .violet600), for: .normal)
+            case .white:
+                self.backgroundColor = .white
+                self.setTitleColor(.violet600, for: .normal)
+                self.setTitleColor(.gray30, for: .disabled)
                 self.layer.borderWidth = 1
-                self.layer.borderColor = UIColor(resource: .violet150).cgColor
+                self.layer.borderColor = UIColor.violet150.cgColor
             }
         } else {
-            font = UIFont.body_2_M
+            self.titleLabel?.font = UIFont.body_2_M
             self.layer.cornerRadius = 5
-            self.setBackgroundColor(UIColor(resource: .violet100))
-            self.setTitleColor(UIColor(resource: .violet800), for: .normal)
+            self.backgroundColor = isEnabled ? .violet100 : .gray30
+            self.setTitleColor(.violet800, for: .normal)
+            self.setTitleColor(.white, for: .disabled)
         }
-        self.titleLabel?.font = font
     }
 }
 
