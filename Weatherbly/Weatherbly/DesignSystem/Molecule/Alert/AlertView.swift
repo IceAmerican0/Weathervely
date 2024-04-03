7ec805084df1eab6ab3a42bac550eb66257b133d
IceAmerican0 <starsjun@naver.com>
2024-04-03 17:27:04 +0900

    [DesignSystem] AlertView 버튼 두개 케이스 추가

--- Weatherbly/Weatherbly/DesignSystem/Molecule/Alert/AlertView.swift
+++ Weatherbly/Weatherbly/DesignSystem/Molecule/Alert/AlertView.swift
@@ -30,11 +30,19 @@ final class AlertView: UIView {
         alignment: .center
     ).make()
     
+    private var buttonContainer = UIView()
+    
     private let confirmButton = UIButton().then {
-        $0.setCornerRadius(16, [.bottomLeft, .bottomRight])
         $0.backgroundColor = .violet600
         $0.titleLabel?.font = .body_1_B
         $0.setTitle("확인", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+    }
+    
+    private let cancelButton = UIButton().then {
+        $0.setCornerRadius(16, [.bottomRight])
+        $0.backgroundColor = .violet600
+        $0.titleLabel?.font = .body_1_B
         $0.setTitleColor(.white, for: .normal)
     }
     
@@ -67,23 +75,62 @@ final class AlertView: UIView {
     }
     
     func layout() {
+        setButtonList()
+        
         flex.addItem(dimView).paddingHorizontal(53).define {
             $0.addItem(alertContainer).justifyContent(.center).alignItems(.center).define {
                 $0.addItem(image).marginTop(34).size(88)
                 $0.addItem(message.then {
                     $0.text = state.title
                 }).marginTop(24).marginHorizontal(20)
-                $0.addItem(confirmButton).marginTop(24).alignSelf(.stretch).height(48)
+                $0.addItem(buttonContainer).marginTop(24).alignSelf(.stretch).height(48)
             }
         }
     }
     
     func bind() {
+        // 버튼 두개일시 왼쪽 버튼
         confirmButton.rx.tap
             .bind(with: self) { owner, _ in
                 owner.dismiss()
-                owner.state.closeAction?()
+                
+                switch owner.state.buttonListState {
+                case .single:
+                    owner.state.closeAction?()
+                case .double(let left, let right):
+                    left.action?()
+                }
             }.disposed(by: bag)
+        
+        cancelButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.dismiss()
+                
+                switch owner.state.buttonListState {
+                case .single:
+                    owner.state.closeAction?()
+                case .double(let left, let right):
+                    right.action?()
+                }
+            }.disposed(by: bag)
+    }
+    
+    func setButtonList() {
+        switch state.buttonListState {
+        case .single:
+            confirmButton.setCornerRadius(16, [.bottomLeft, .bottomRight])
+            buttonContainer.flex.addItem(confirmButton).grow(1)
+        case .double(let left, let right):
+            confirmButton.setCornerRadius(16, [.bottomLeft])
+            confirmButton.setTitle(left.title, for: .normal)
+            cancelButton.setTitle(right.title, for: .normal)
+            
+            buttonContainer.flex.direction(.row).define {
+                $0.addItem(confirmButton).basis(0).grow(1)
+                $0.addItem().backgroundColor(.white).width(1)
+                $0.addItem(cancelButton).basis(0).grow(1)
+            }
+        }
     }
     
     public func dismiss() {
